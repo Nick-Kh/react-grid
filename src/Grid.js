4e57be5711fded361c70ffafe3a1eb7a984043d6
Nick-Kh <many_sided@hotmail.com>
2021-10-11 22:34:13 +0300

    grid table implementation

--- src/Grid.js
+++ src/Grid.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Grid = ({ config, data }) => (
-  <table>
-    <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    </tbody>
-  </table>
-);
+const Grid = ({ config, data }) => {
 
+  const ENTRIES_PER_PAGE = 10;
+  const [currPage, setCurrPage] = useState(0);
+  const [currData, setCurrData] = useState(data.slice(0, ENTRIES_PER_PAGE))
+  const pageNum = Math.ceil(data.length  / ENTRIES_PER_PAGE);
+  const dataFields = config.map(data => data.field);
+
+  const setPageAndData = (idx) => {
+    if(idx < 0 || idx === pageNum) return;
+    setCurrPage(idx);
+    const startIndex = idx * ENTRIES_PER_PAGE;
+    setCurrData(data.slice(startIndex, startIndex + ENTRIES_PER_PAGE));
+  }
+
+  const headerData = config.map((col, idx) => {
+    return <th key={idx}>{col.title}</th>
+  });
+
+  const bodyData = currData.map((row,idx) => {
+    return (
+      <tr key={idx}>
+        {dataFields.map((field, idx) => {
+          if(config[idx].component) {
+            return <td>{config[idx].component({data: row[field]})}</td> 
+          }
+          return <td key={idx}>{row[field]}</td>
+        })}
+      </tr>
+    );
+  });
+  const pages = Array.from({ length: pageNum }, (_, i) => i + 1).map(idx => {
+    return (
+      <span key={idx} onClick={() => {setPageAndData(idx - 1)}} className={ currPage === idx - 1 ? 'selected' : '' }>
+        {idx}
+      </span>
+    );
+  });
+
+  return (
+    <div className="table-container">
+       <table>
+        <thead>
+          <tr>
+            {headerData}
+          </tr>
+        </thead>
+        <tbody>
+          {bodyData}
+        </tbody>
+      </table>
+      <div className="pages-container">
+          <span onClick={() => setPageAndData(currPage - 1)} className={ currPage === 0 ? 'disabled' : ''}>Prev</span>
+          {pages}
+          <span onClick={() => setPageAndData(currPage + 1)} className={ currPage === pageNum - 1 ? 'disabled' : ''}>Next</span>
+      </div>
+    </div>
+   
+  );
+}
+ 
 export default Grid;
